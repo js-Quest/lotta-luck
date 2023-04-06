e78cdeadae40082ebe99d4524f6d1ddf193c2152
JASaddington <jasaddington@gmail.com>
2023-04-06 16:57:20 -0500

    still error-ing out i dev tools. have a tutor appt now.  stay tuned for next commit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ console.log("ding");
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var characters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "<", ">", "?"];
-var numbers = [0,1,2,3,4,5,6,7,8,9];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
 
 // get criteria for rest of password according to the bootcamp guidelines
@@ -23,15 +23,6 @@ function allOptions() {
   var wantUppercase = window.confirm("Do you want to use upper-case letters? (click OK if you do, CANCEL for nope)");
   var wantNumbers = window.confirm("Do you want to use numbers? (click OK if you do, CANCEL for nope)");
   var wantCharacters = window.confirm("Do you want to use special characters? (click OK if you do, CANCEL for nope)");
-  var chosenOptions = {
-    pswdLength: pswdLength,
-    wantLowercase: wantLowercase,
-    wantUppercase: wantUppercase,
-    wantNumbers: wantNumbers,
-    wantCharacters: wantCharacters
-  }
-
-
 
   // check to make sure one type of character is chosen
   if (
@@ -39,25 +30,23 @@ function allOptions() {
     wantUppercase === false &&
     wantNumbers === false &&
     wantCharacters === false)
-    window.alert("User must select at least one type of character");
+    window.alert("User must select at least one type of character, please try again.");
   else if(pswdLength < 8 || pswdLength > 128) {
     window.alert("Required password length is 8 - 128 characters, please try again.");
   }
-  else if (Number.isNaN(pswdLength)) {
-    window.alert("Password length must be a numeric response")
+  else if (pswdLength = NaN) {
+    window.alert("Password length must be a numeric response, please try again.")
   }
   else 
     isValid = true;
+
 }
 
+// pull random items from arrays and compile them according to user choices
 function generatePassword() {
   var answers = allOptions();
   var compiledPswd = [];
   var userPassword = "";
-
-  // var randomLowercase = [Math.floor(Math.random() * lowerCase.length)];
-  // var chosenLowercase = lowerCase[randomLowercase]
-  // console.log(chosenLowercase);
 
   if (answers.wantLowercase) {
     for (var x of lowerCase)
@@ -69,7 +58,7 @@ function generatePassword() {
     compiledPswd.push(x);
   }
 
-  if (answers.wantNumbers) {
+  if (answers.wantCharacters) {
     for (var x of numbers)
       compiledPswd.push(x);
   }
@@ -79,25 +68,14 @@ function generatePassword() {
       compiledPswd.push(x);
   }
 
-  for (var x = 0; x < answers.length; x++) {
-    userPassword + compiledPswd[Math.floor(Math.random() * compiledPswd.length)];
+  for (x = compiledPswd.length - 1; x < pswdLength; x++) {
+    userPassword += compiledPswd[Math.floor(Math.random() * compiledPswd.length)];
   }
-
-
-  // var randomLowercase = [Math.floor(Math.random() * lowerCase.length)];
-  // var chosenLowercase = lowercase[randomLowercase]
-  // var chosenUppercase = [Math.floor(Math.random() * upperCase.length)];
-  // var chosenNumbers = [Math.floor(Math.random() * numbers.length)];
-  // var chosenChars = [Math.floor(Math.random() * characters.length)];
-
-
-
-  
-
-
 
   return userPassword;
 }
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
